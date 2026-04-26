df1b7ed6aff3371f16526994215bf3edea529be9
Kiichi funatu <magiciansred7@icloud.com>
2026-04-26 22:56:48 +0900

    4/26 会員登録画面作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/register', function () {
+    return view('auth.register');
 });
